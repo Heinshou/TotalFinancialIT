fc34bdaa7161d1248e2ce4b32ce673e6e2b0ea13
Miguel <miguelecchavez@gmail.com>
2023-04-19 12:54:28 -0500

    m

--- Components/SecondSection/SecondSection.jsx
+++ Components/SecondSection/SecondSection.jsx
@@ -21,62 +21,36 @@ const SecondSection = () => {
                         <RiMedalLine className='card__icon card__icon__resolution' />
                         <h2 className='front__card__title'>Experiencia</h2>
                     </div>
-                    {/* <div className='big__back__card card_one'>
-                        <p> Experiencia en proyectos tecnológicos para tomar mejores decisiones,
-                            potencializar resultados y financiar proyectos. </p>
-                    </div> */}
                 </div>
                 <div className='cards__sections2'>
                     <div className='front__card card__one'>
                         <MdSecurity className='card__icon' />
                         <h2 className='front__card__title'>Seguridad y Confianza</h2>
                     </div>
-                    {/* <div className='big__back__card card_one'>
-                        <p> La entidad ofrece confiabilidad y experiencia para ser una pieza clave en proyectos,
-                            eliminando la burocracia y disminuyendo riesgos.</p>
-                    </div> */}
                 </div>
                 <div className='cards__sections3'>
                     <div className='front__card card__one'>
                         <AiOutlineRise className='card__icon' />
                         <h2 className='front__card__title'>Impulsamos el crecimiento</h2>
                     </div>
-                    {/* <div className='big__back__card card_one'>
-                        <p>La empresa ofrece financiamiento para adquirir o renovar activos de hardware informático
-                            que optimicen la empresa o al personal.</p>
-                    </div> */}
                 </div>
                 <div className='cards__sections4'>
                     <div className='front__card card__one'>
                         <BiRun className='card__icon card__icon__resolution' />
                         <h2 className='front__card__title'>Agilidad</h2>
                     </div>
-                    {/* <div className='back__card card_one'>
-                        <p> Contamos con atención personalizada, respuesta rápida y
-                            solución a tus necesidades.</p>
-                    </div> */}
                 </div>
                 <div className='cards__sections5'>
                     <div className='front__card card__one'>
                         <AiOutlineAppstoreAdd className='card__icon' />
                         <h2 className='front__card__title6'>Implementción de Nuevos Sistemas</h2>
                     </div>
-                    {/* <div className='back__card card_one'>
-                        <p> Invierte en innovación y desarrolla o implementa
-                            productos y servicios nuevos de tecnologia para
-                            dar mas herramientas a tu negocio.</p>
-                    </div> */}
                 </div>
                 <div className='cards__sections6'>
                     <div className='front__card card__one'>
                         <BsFillPeopleFill className='card__icon' />
                         <h2 className='front__card__title6'>Equipo de Profesionales</h2>
                     </div>
-                    {/* <div className='back__card card_one'>
-                        <p> Especialistas en el otorgamiento de asesoría
-                            financiera y tecnologica para que tu inversión te de
-                            mas por tu dinero.</p>
-                    </div> */}
                 </div>
             </div>
         </div>
